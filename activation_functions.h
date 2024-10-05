e128aea6eca0c8e7f89075ce05297e9372250c63
NotJakeFromStateFarm <cyb3rjake@gmail.com>
2024-10-05 17:43:12 -0400

    Create activation_functions.h

---
+++ activation_functions.h
@@ -0,0 +1,9 @@
+#ifndef ACTIVATION_FUNCTIONS_H
+#define ACTIVATION_FUNCTIONS_H
+
+double sigmoid(double x);
+double sigmoid_derivative(double x);
+double relu(double x);
+double relu_derivative(double x);
+
+#endif
