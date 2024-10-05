a797611bb37426f9f4294a3a76a172ab9654be03
NotJakeFromStateFarm <cyb3rjake@gmail.com>
2024-10-05 17:43:46 -0400

    Create dataset.c

---
+++ dataset.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dataset.h"
+
+Dataset* load_dataset(const char *file_path) {
+    // Implementation omitted for brevity
+    return NULL;
+}
+
+void free_dataset(Dataset *dataset) {
+    // Implementation omitted for brevity
+}
