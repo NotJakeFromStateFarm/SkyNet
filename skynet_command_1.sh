053d92885d941497cdda5a8c5b23d35a6848821a
NotJakeFromStateFarm <cyb3rjake@gmail.com>
2024-11-09 14:46:44 -0500

    Update skynet_command_1.sh

--- skynet_command_1.sh
+++ skynet_command_1.sh
@@ -1,1 +1,2 @@
-echo $(echo 'aHR0cHM6Ly9tZWdhLm56L2ZpbGUv' | base64 -d)$(echo 'c0k0UUhaYUw=' | base64 -d)$(echo 'I1BfaW1pbExTSmJLYk1nNHd6Mm5BOHVMbDJrYU9hekNZUy1nTVFYX01DTHc=' | base64 -d)
+$part1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9tZWdhLm56L2ZpbGUvc0k0UUhaYUw="))
+Write-Output $part1
